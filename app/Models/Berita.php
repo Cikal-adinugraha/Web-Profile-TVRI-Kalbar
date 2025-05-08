15e6cffac7c439fb6c9bee4c686d9cac06c6fa96
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-09 03:23:52 +0700

    Add Berita model and migration for berita table

---
+++ app/Models/Berita.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Berita extends Model
+{
+    /** @use HasFactory<\Database\Factories\BeritaFactory> */
+    use HasFactory, Sluggable;
+
+    protected $table = 'berita';
+    protected $guarded = ["id"];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'judul'
+            ]
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
