a27e02faf512b76f50eeb8ad96be4377a894de6d
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-09 03:24:16 +0700

    Add BeritaFactory and BeritaSeeder for seeding berita data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([BeritaSeeder::class]);
+
         User::factory()->create([
             'name' => 'admin',
             'username' => 'admin',
