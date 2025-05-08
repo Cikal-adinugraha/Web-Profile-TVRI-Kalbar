5d6702e0a27d0f16d1e999246d62200a50b12a1d
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-08 16:41:59 +0700

    Update web routes to return index view and organize authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("index");
+});
+
+Route::middleware("guest")->group(function () {
+    Route::get("/login", [AuthController::class, "index"])->name("login.get");
+    Route::post("/login", [AuthController::class, "login"])->name("login.post");
+});
+
+Route::middleware("auth")->group(function () {
+    Route::get("/dashboard", fn() => view("admin.dashboard"))->name("dashboard");
+
+    Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 });
