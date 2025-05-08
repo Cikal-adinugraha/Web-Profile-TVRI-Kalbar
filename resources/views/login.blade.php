033ae90c8205159d806c559aec1d1c35a78ba2d9
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-08 16:42:07 +0700

    Add authentication controller and views for login and logout functionality

---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+@extends('root')
+
+@section('content')
+  <div class="container">
+    <h1>login atmin</h1>
+    <p>ini form loginnya</p>
+    <div>
+      <form method="POST" action="{{ route('login.post') }}">
+        @csrf
+        @session('failed')
+          <p class="text-2xl text-red-500">
+            {{ session('failed') }}
+          </p>
+        @endsession
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" required class="border border-black">
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" required class="border border-black">
+        </div>
+        <div>
+          <button type="submit" class="border border-black px-4 py-2">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+@endsection
