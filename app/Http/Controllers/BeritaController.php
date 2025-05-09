e4fc5d8d1b1480cd96f1122f8119d7441cef751b
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-10 00:56:43 +0700

    Implement BeritaController for news management with create, store, and show functionalities

---
+++ app/Http/Controllers/BeritaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Berita;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\ImageFile;
+
+class BeritaController extends Controller
+{
+    public function index()
+    {
+        return view("admin.dashboard", [
+            "news" => Berita::latest()->get(),
+        ]);
+    }
+
+    public function create()
+    {
+        return view("admin.berita.tambah");
+    }
+
+    public function store(Request $request)
+    {
+        $data = collect($request->validate([
+            "judul" => "required|string",
+            "isi" => "required|string",
+            "gambar" => ImageFile::image()->max("3mb")->extensions(["jpg", "jpeg", "png"])->rules(["nullable"]),
+        ]));
+
+        if ($request->hasFile("gambar")) {
+            $data->put("gambar", $request->file("gambar")->store("images/berita"));
+        }
+
+        Berita::create([
+            ...$data->toArray(),
+            "user_id" => Auth::user()->id,
+        ]);
+
+        return redirect()->route("dashboard");
+    }
+
+    public function show(Berita $berita)
+    {
+        return view("berita", [
+            "berita" => $berita,
+        ]);
+    }
+}
