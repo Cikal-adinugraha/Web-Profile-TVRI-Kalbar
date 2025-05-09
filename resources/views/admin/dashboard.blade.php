9a848657407f474b29218e0fa3024766c1458ec8
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-10 00:56:33 +0700

    Update dashboard view for improved news display and management

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,50 @@
-@extends('index')
+@extends('root')
 
 @section('content')
-  <h1>ini halaman dashboard atmin</h1>
-  <h2>cuma bisa diakses kalo user dah login</h2>
-  <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="border border-black px-4 py-2">Logout</button>
-  </form>
+  <div class="container p-4">
+    <h1>ini halaman dashboard atmin</h1>
+    <form method="POST" action="{{ route('logout') }}">
+      @csrf
+      <button type="submit" class="border border-black px-4 py-2">Logout</button>
+    </form>
+    <div class="mt-4">
+      <a href="{{ route('berita.tambah') }}">Berita Baru</a>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Gambar</th>
+            <th>Judul</th>
+            <th>Tanggal</th>
+            <th>Status</th>
+            <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($news as $berita)
+            <tr>
+              <td class="px-6">{{ $loop->iteration }}</td>
+              <th class="px-6">
+                <img src="{{ asset($berita->gambar ? 'storage/' . $berita->gambar : 'images/perpus.jpg') }}" alt="Gambar" class="h-40 w-56 object-cover">
+              </th>
+              <th class="px-6">{{ $berita->judul }}</th>
+              <th class="px-6">{{ $berita->created_at->addHours(7)->translatedFormat('d F Y h:i:s') }}</th>
+              <th class="px-6">{{ $berita->is_published ? 'Sudah Dipublish' : 'Belum Dipublish' }}</th>
+              <th class="px-6">
+                <div>
+                  preview
+                </div>
+                <div>
+                  edit
+                </div>
+                <div>
+                  hapus
+                </div>
+              </th>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
 @endsection
