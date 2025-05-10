0586dfd1c838397c7a25de3e7decb3b3614b7fdd
rikarani <46773591+rikarani@users.noreply.github.com>
2025-05-10 15:11:08 +0700

    Reorder routes for news articles to improve organization and clarity

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Models\Berita;
 use App\Http\Controllers\BeritaController;
 
 Route::get('/', fn() => view("index", ["news" => Berita::published()->latest()->get()]));
-Route::get("/berita/{berita}", [BeritaController::class, "show"])->name("berita.show");
 
 Route::middleware("guest")->group(function () {
     Route::get("/login", [AuthController::class, "index"])->name("login.get");
@@ -27,8 +26,14 @@ Route::middleware("auth")->group(function () {
     Route::put("/berita/publish/{berita}", [BeritaController::class, "publish"])->name("berita.publish");
     Route::put("/berita/unpublish/{berita}", [BeritaController::class, "unpublish"])->name("berita.unpublish");
 
+    // * Edit Berita
+    Route::get("/berita/edit/{berita}", [BeritaController::class, "edit"])->name("berita.edit");
+    Route::put("/berita/edit/{berita}", [BeritaController::class, "update"])->name("berita.update");
+
     // * Hapus Berita
     Route::delete("/berita/{berita}", [BeritaController::class, "destroy"])->name("berita.destroy");
 
     Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 });
+
+Route::get("/berita/{berita}", [BeritaController::class, "show"])->name("berita.show");
